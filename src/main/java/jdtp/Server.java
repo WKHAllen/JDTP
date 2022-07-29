a23951c21071f18fdbe384a9208defef94cc54cb
WKHAllen <wkhallen@gmail.com>
2022-07-28 20:57:57 -0400

    Implement server serving

--- src/main/java/jdtp/Server.java
+++ src/main/java/jdtp/Server.java
@@ -2,18 +2,21 @@ package jdtp;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.HashMap;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.*;
 
 public abstract class Server {
     private final boolean blocking;
     private final boolean eventBlocking;
     private boolean serving = false;
-    private ServerSocket sock = null;
+    private Selector selector = null;
+    private ServerSocketChannel sock = null;
     private Thread serveThread = null;
-    private HashMap<Long, Socket> clients = new HashMap<Long, Socket>();
+    private final HashMap<Long, SocketChannel> clients = new HashMap<Long, SocketChannel>();
     private long nextClientID = 0;
 
     public Server(boolean blocking_, boolean eventBlocking_) {
@@ -25,91 +28,148 @@ public abstract class Server {
         this(false, false);
     }
 
-    public void start(String host, int port) throws JDTPException, UnknownHostException, IOException {
+    public void start(String host, int port) throws JDTPException, IOException {
         if (serving) {
             throw new JDTPException("server is already serving");
         }
 
         InetSocketAddress address = new InetSocketAddress(host, port);
 
-        sock = new ServerSocket();
+        selector = Selector.open();
+
+        sock = ServerSocketChannel.open();
         sock.bind(address, Util.listenBacklog);
+        sock.configureBlocking(false);
+        sock.register(selector, SelectionKey.OP_ACCEPT);
 
         serving = true;
         callServe();
     }
 
-    public void start(String host) {
-        // TODO
+    public void start(String host) throws JDTPException, IOException {
+        start(host, Util.defaultPort);
     }
 
-    public void start(int port) {
-        // TODO
+    public void start(int port) throws JDTPException, IOException {
+        start(Util.defaultHost(), port);
     }
 
-    public void start() {
-        // TODO
+    public void start() throws JDTPException, IOException {
+        start(Util.defaultHost(), Util.defaultPort);
     }
 
-    public void stop() {
-        // TODO
+    public void stop() throws JDTPException, IOException, InterruptedException {
+        if (!serving) {
+            throw new JDTPException("server is not serving");
+        }
+
+        serving = false;
+
+        for (Map.Entry<Long, SocketChannel> client : clients.entrySet()) {
+            removeClient(client.getKey());
+        }
+
+        sock.close();
+
+        if (serveThread != null && serveThread != Thread.currentThread()) {
+            serveThread.join();
+        }
+
+        selector.close();
     }
 
-    public void send(long clientID, byte[] data) {
-        // TODO
+    public void send(long clientID, byte[] data) throws JDTPException, IOException {
+        if (!serving) {
+            throw new JDTPException("server is not serving");
+        }
+
+        SocketChannel clientSock = clients.get(clientID);
+
+        if (clientSock != null) {
+            byte[] encodedData = Util.encodeMessage(data);
+            ByteBuffer encodedDataBuffer = ByteBuffer.wrap(encodedData);
+            clientSock.write(encodedDataBuffer);
+        } else {
+            throw new JDTPException("client does not exist");
+        }
     }
 
-    public void sendAll(byte[] data) {
-        // TODO
+    public void sendAll(byte[] data) throws JDTPException, IOException {
+        if (!serving) {
+            throw new JDTPException("server is not serving");
+        }
+
+        byte[] encodedData = Util.encodeMessage(data);
+
+        for (Map.Entry<Long, SocketChannel> client : clients.entrySet()) {
+            SocketChannel clientSock = client.getValue();
+            ByteBuffer encodedDataBuffer = ByteBuffer.wrap(encodedData);
+            clientSock.write(encodedDataBuffer);
+        }
     }
 
-    public void removeClient(long clientID) {
-        // TODO
+    public void removeClient(long clientID) throws JDTPException, IOException {
+        if (!serving) {
+            throw new JDTPException("server is not serving");
+        }
+
+        SocketChannel client = clients.get(clientID);
+
+        if (client != null) {
+            client.close();
+            clients.remove(clientID);
+        } else {
+            throw new JDTPException("client does not exist");
+        }
     }
 
     public boolean isServing() {
         return serving;
     }
 
-    public String getHost() throws JDTPException {
+    public String getHost() throws JDTPException, IOException {
         if (!serving) {
             throw new JDTPException("server is not serving");
         }
 
-        return sock.getInetAddress().getHostAddress();
+        InetSocketAddress address = (InetSocketAddress) sock.getLocalAddress();
+        return address.getAddress().getHostAddress();
     }
 
-    public int getPort() throws JDTPException {
+    public int getPort() throws JDTPException, IOException {
         if (!serving) {
             throw new JDTPException("server is not serving");
         }
 
-        return sock.getLocalPort();
+        InetSocketAddress address = (InetSocketAddress) sock.getLocalAddress();
+        return address.getPort();
     }
 
-    public String getClientHost(long clientID) throws JDTPException {
+    public String getClientHost(long clientID) throws JDTPException, IOException {
         if (!serving) {
             throw new JDTPException("server is not serving");
         }
 
-        Socket client = clients.get(clientID);
+        SocketChannel client = clients.get(clientID);
 
         if (client != null) {
-            return client.getInetAddress().getHostAddress();
+            InetSocketAddress address = (InetSocketAddress) client.getRemoteAddress();
+            return address.getAddress().getHostAddress();
         } else {
             throw new JDTPException("client does not exist");
         }
     }
 
-    public int getClientPort(long clientID) throws JDTPException {
+    public int getClientPort(long clientID) throws JDTPException, IOException {
         if (!serving) {
             throw new JDTPException("server is not serving");
         }
 
-        Socket client = clients.get(clientID);
+        SocketChannel client = clients.get(clientID);
 
         if (client != null) {
-            return client.getPort();
+            InetSocketAddress address = (InetSocketAddress) client.getRemoteAddress();
+            return address.getPort();
         } else {
             throw new JDTPException("client does not exist");
         }
@@ -119,24 +179,101 @@ public abstract class Server {
         return nextClientID++;
     }
 
-    private void callServe() {
-        // TODO
+    private void callServe() throws IOException {
+        if (blocking) {
+            serve();
+        } else {
+            serveThread = new Thread(() -> {
+                try {
+                    serve();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            serveThread.start();
+        }
     }
 
-    private void serve() {
-        // TODO
+    private void serve() throws IOException {
+        while (serving) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+
+                if (key.isAcceptable()) {
+                    SocketChannel client = sock.accept();
+                    client.configureBlocking(false);
+                    client.register(selector, SelectionKey.OP_READ);
+
+                    long clientID = newClientID();
+                    clients.put(clientID, client);
+
+                    callConnect(clientID);
+                } else if (key.isReadable()) {
+                    SocketChannel client = (SocketChannel) key.channel();
+                    long clientID = clients.entrySet().stream().filter(entry -> Objects.equals(entry.getValue(), client)).map(Map.Entry::getKey).toList().get(0);
+                    ByteBuffer sizeBuffer = ByteBuffer.allocate(Util.lenSize);
+
+                    int bytesReceived = client.read(sizeBuffer);
+
+                    if (bytesReceived != Util.lenSize) {
+                        if (clients.containsKey(clientID)) {
+                            client.close();
+                            clients.remove(clientID);
+
+                            callDisconnect(clientID);
+                            continue;
+                        }
+                    }
+
+                    long messageSize = Util.decodeMessageSize(sizeBuffer.array());
+                    ByteBuffer messageBuffer = ByteBuffer.allocate((int) messageSize);
+
+                    bytesReceived = client.read(messageBuffer);
+
+                    if (bytesReceived != messageSize) {
+                        if (clients.containsKey(clientID)) {
+                            client.close();
+                            clients.remove(clientID);
+
+                            callDisconnect(clientID);
+                            continue;
+                        }
+                    }
+
+                    callReceive(clientID, messageBuffer.array());
+                }
+
+                iter.remove();
+            }
+        }
     }
 
     private void callReceive(long clientID, byte[] data) {
-        // TODO
+        if (eventBlocking) {
+            receive(clientID, data);
+        } else {
+            new Thread(() -> receive(clientID, data)).start();
+        }
     }
 
     private void callConnect(long clientID) {
-        // TODO
+        if (eventBlocking) {
+            connect(clientID);
+        } else {
+            new Thread(() -> connect(clientID)).start();
+        }
     }
 
     private void callDisconnect(long clientID) {
-        // TODO
+        if (eventBlocking) {
+            disconnect(clientID);
+        } else {
+            new Thread(() -> disconnect(clientID)).start();
+        }
     }
 
     protected abstract void receive(long clientID, byte[] data);
--- src/main/java/jdtp/Util.java
+++ src/main/java/jdtp/Util.java
@@ -46,7 +46,6 @@ class Util {
         int aLen = a.length;
         int bLen = b.length;
 
-        @SuppressWarnings("unchecked")
         T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
         System.arraycopy(a, 0, c, 0, aLen);
         System.arraycopy(b, 0, c, aLen, bLen);
